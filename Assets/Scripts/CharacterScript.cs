5fc3669ef5144c4870065dce4f6ee00d2c3c60c9
Amy He <amyh11325@gmail.com>
2021-12-16 15:25:55 -0800

    add animation to movement, add extra frame to puke anim

--- Assets/Scripts/CharacterScript.cs
+++ Assets/Scripts/CharacterScript.cs
@@ -5,9 +5,21 @@ using UnityEngine;
 public class CharacterScript : MonoBehaviour
 {
     public float speed = 3f;
+    public Animator animator;
+    SpriteRenderer m_SpriteRenderer;
+
+    void Start()
+    {
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     void Update()
     {
-        this.transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0, 0);
+        float horizontalMove = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        this.transform.Translate(horizontalMove, 0, 0);
+
+        // animator
+        m_SpriteRenderer.flipX = (horizontalMove < 0);
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
     }
 }
