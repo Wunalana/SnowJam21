cfd4f0d83ffc05fbac2701653a0a0df9f6a503a4
Kaustuv Prateeq <kaustuvprateeq200@gmail.com>
2021-12-16 15:17:24 -0800

    KP05 Spawner spawns at random time intervals
    Bugfixes: Fixed Random.Range to encompass the whole of foods arrray

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public float spawn_freq = 1f;
+    public float spawn_freq = 0.25f; 
     public GameObject[] foods; //array of food sprites
     public GameObject spawner; //this
     public float spawn_range = 3f; //how far from spawner can food spawn
-    private int numFoods; //number of foods to throw
+    public int numFoods; //number of foods to throw
 
     void Start()
     {
@@ -24,22 +24,24 @@ public class Spawner : MonoBehaviour
 
     IEnumerator SpeedScaler()
     {
+        float randomTimeFactor = Random.Range(1, 4);
         for (; ; )
         {
             // execute block of code here
             spawnBlock();
-            yield return new WaitForSeconds(spawn_freq);
+            yield return new WaitForSeconds(spawn_freq * randomTimeFactor);
         }
     }
 
     void spawnBlock()
     {
         float randomNumber = Random.Range(-spawn_range, spawn_range);
-        int randomFood = Random.Range(0, numFoods - 1);
+        int randomFood = Random.Range(0, numFoods);
+
 
         Instantiate(
             foods[randomFood], new Vector3(
-            spawner.transform.position.x + randomNumber, spawner.transform.position.y, spawner.transform.position.z), 
+            this.transform.position.x + randomNumber, this.transform.position.y, this.transform.position.z), 
             Quaternion.identity);
 
     }
