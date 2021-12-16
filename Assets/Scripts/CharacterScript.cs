2477ed524c21af906eacf380157a77166ced48b5
Kaustuv Prateeq <kaustuvprateeq200@gmail.com>
2021-12-16 14:18:18 -0800

    KP02 Edited Character Script, Changed spawner to local
    Spawner now spawns around it instead of magic number, Character movement reduced to a macro, added sprite images to character

--- Assets/Scripts/CharacterScript.cs
+++ Assets/Scripts/CharacterScript.cs
@@ -4,63 +4,10 @@ using UnityEngine;
 
 public class CharacterScript : MonoBehaviour
 {
-    Vector2 _initialPos;
-    Rigidbody2D _rb;
+    public float speed = 3f;
 
-    // DEFINE VELOCITY VALUES
-    Vector2 rightVelocity;
-    Vector2 leftVelocity;
-    Vector2 downVelocity;
-
-    Vector2 testVelocity;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        _rb = GetComponent<Rigidbody2D>();
-        // transform.position = _initialPos;
-
-        // INITIALIZE VELOCITY VALUES
-        rightVelocity = new Vector2(7, 0);
-        leftVelocity = new Vector2(-7, 0);
-        testVelocity = new Vector2(0, 0);
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.D))
-        {
-            Debug.Log("D key was pressed");
-            moveRight();
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            Debug.Log("A key was pressed");
-            moveLeft();
-        }
-
-
+        this.transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0, 0);
     }
-
-    private void moveRight()
-    {
-        if (_rb.velocity.x < rightVelocity.x)
-        {
-            _rb.AddForce(rightVelocity);
-        }
-
-    }
-
-    private void moveLeft()
-    {
-        if (_rb.velocity.x > leftVelocity.x)
-        {
-            _rb.AddForce(leftVelocity);
-        }
-    }
-
-
 }
